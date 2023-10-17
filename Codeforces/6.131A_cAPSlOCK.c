e8f07ac7dcf9c495e6c3eb8f3d5a911397dd91d3
Nur Hossain <tnurhossain@gmail.com>
2023-10-18 02:35:51 +0600

    Codeforces

---
+++ Codeforces/6.131A_cAPSlOCK.c
@@ -0,0 +1,41 @@
+//TheDark ===>> 23 June 2022
+//Checking CapsLock
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[105];
+    scanf("%s",str);
+
+    //CHECK 1ST LOWER CASE AND REST ARE UPPER CASE
+    if(str[0]>='a' && str[0]<='z')
+    {
+        for(int i=1;i<strlen(str);i++)
+        {
+
+            if(str[i]>='a' && str[i]<='z')
+            {
+                printf("%s",str);
+                return 0;
+            }
+        }
+        strlwr(str);
+        str[0]=str[0] - 32;
+        printf("%s",str);
+        return 0;
+    }
+
+    //CHECK ALL UPPER CASE
+    for(int i=0;i<strlen(str);i++)
+    {
+
+        if(str[i]>='a' && str[i]<='z')
+        {
+            printf("%s",str);
+            return 0;
+        }
+    }
+    strlwr(str);
+    printf("%s",str);
+    return 0;
+}
