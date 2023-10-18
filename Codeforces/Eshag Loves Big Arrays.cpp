116b42d6d962797501d2ece9bfdc194bad63c9a7
Nur Hossain <tnurhossain@gmail.com>
2023-10-19 01:02:16 +0600

    Codeforces

---
+++ Codeforces/Eshag Loves Big Arrays.cpp
@@ -0,0 +1,34 @@
+//The Dark ----> 25 August 2023
+// Eshag Loves Big Arrays
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t,n,x,v=0;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        int arr[101]={0};
+        for(int i=0;i<n;i++)
+        {
+            cin>>x;
+            arr[x]++;
+        }
+        for(int i=1;i<101;i++)
+        {
+            if(arr[i]>0)
+            {
+                if(arr[i]==n)
+                    v=n;
+                else
+                {
+                    v=arr[i];
+                    break;
+                }
+            }
+        }
+        cout<<n-v<<endl;
+    }
+    return 0;
+}
